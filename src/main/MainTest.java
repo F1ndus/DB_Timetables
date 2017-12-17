35ce8f9cfcb703983c3a5b20014a4de2e831eab7
Philipp <PhilippHentschel@t-online.de>
2017-12-17 14:27:47 +0100

    Formatting

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -1,14 +1,24 @@
 package main;
 
 import main.generated.Timetable;
+import main.generated.TimetableStop;
+import org.fusesource.jansi.Ansi;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import static main.Fahrplan.download24HourTimeTable;
+import static org.fusesource.jansi.Ansi.Color.RED;
+import static org.fusesource.jansi.Ansi.ansi;
 
 /**
  * Created by Philipp on 16.12.2017.
@@ -20,8 +30,14 @@ public class MainTest {
         int evaidberlin = 8011160;
         int evaidhamburg = 8002549;
         LocalDateTime date = LocalDateTime.now();
-        Timetable timetable = download24HourTimeTable(date,evaidhamburg);
-        timetable.getS().sort((a, b) -> {
+        Timetable timetable = download24HourTimeTable(date,evaidbs);
+        List<TimetableStop> stopList  = timetable.getS()
+                .stream()
+                .distinct()
+                .filter(entry -> entry.getTl().getC().matches("IC.*"))
+                .collect(Collectors.toList());
+
+        stopList.sort((a, b) -> {
             String cmpA = a.getAr() != null? a.getAr().getPt(): a.getDp().getPt();
             String cmpB = b.getAr() != null? b.getAr().getPt(): b.getDp().getPt();
             return LocalDateTime.parse(cmpA,formatter).compareTo(LocalDateTime.parse(cmpB,formatter));
@@ -29,7 +45,9 @@ public class MainTest {
 
 
         AtomicReference<LocalDateTime> lastHour = new AtomicReference<>(LocalDateTime.now().minusYears(1000));
-        timetable.getS().stream().forEach(stop -> {
+        int longestarrivaltrip = getLongestTripToStation(stopList);
+        String formatString = ansi().reset().a("           Arr: %11s ~> Dp: %11s %5s %5s Gl. %6s").fgBright(Ansi.Color.BLACK).a(" %"+longestarrivaltrip+"s ").reset().bold().a("Braunschweig ").reset().fgBright(Ansi.Color.BLACK).a("%s").reset().toString();
+        stopList.stream().forEach(stop -> {
             LocalDateTime thisHour = stop.getAr() != null?
                     LocalDateTime.parse(stop.getAr().getPt(),formatter):
                     LocalDateTime.parse(stop.getDp().getPt(),formatter);
@@ -41,16 +59,23 @@ public class MainTest {
                 lastHour.set(thisHour);
             }
 
-            String name = String.format("           Ankunft: %11s   Abfahrt: %11s %5s %5s Gl. %6s %s",
-                    stop.getAr() != null? LocalDateTime.parse(stop.getAr().getPt(),formatter).format(DateTimeFormatter.ofPattern("dd.MM HH:mm")):"x",
-                    stop.getDp() != null?LocalDateTime.parse(stop.getDp().getPt(),formatter).format(DateTimeFormatter.ofPattern("dd.MM HH:mm")):"x",
+            String name = String.format(formatString,
+                    stop.getAr() != null? LocalDateTime.parse(stop.getAr().getPt(),formatter).format(DateTimeFormatter.ofPattern("dd.MM-HH:mm")):"x",
+                    stop.getDp() != null?LocalDateTime.parse(stop.getDp().getPt(),formatter).format(DateTimeFormatter.ofPattern("dd.MM-HH:mm")):"x",
                     stop.getTl().getC(),
                     stop.getTl().getN(),
                     stop.getAr() != null? stop.getAr().getPp():stop.getDp() != null? stop.getDp().getPp():"x",
+                    stop.getAr() != null? stop.getAr().getPpth():"x",
                     stop.getDp() != null ? stop.getDp().getPpth():"x");
             System.out.println(name);
         });
 
+    }
+
+    private static int getLongestTripToStation(List<TimetableStop> stops) {
+        return stops.stream().filter(stop -> Objects.nonNull(stop.getAr()))
+                .map(stop -> stop.getAr().getPpth())
+                .max(Comparator.comparingInt(String::length)).orElse("").length();
     }
 
 }
